4de7523be313c057a1a5ce3905f534769a017b99
cherjios <slopez0286@gmail.com>
2020-08-18 10:46:29 -0700

    Adding text to about myself

--- portfolio/src/pages/About/index.js
+++ portfolio/src/pages/About/index.js
@@ -18,11 +18,10 @@ function About() {
                     <div className="row">
                         <div className="col">
                             <img src={selfi} className="thisImage" alt="about-me" />
-                            <p className="ptext">Hi! My Name is <span className="text-success font-weight-bold">Sergio Lopez </span>, a software engineer base in the bay area. </p>
-                            <p> I decided to get into the tech word because I like to challenge myself with new projects and I love  spend time with computers, so I decided to learn more about how to create softwares and applications on internet.</p>
-                            <p className="ptext">I am currently studying the web developer boot camp at Berkeley Extension, I'll finish it at the end of August 2020.</p>
-                            <p className="ptext">I finished the fundamental year at Holberton. I was trained in software engineering fundamentals, including low-level programming, DevOps, and high-level modern languages.</p>
-                            <p className="ptext">I am current looking for an entry-level position where I can find opportunities to expand my knowledge and work on challenging projects.</p>
+                            <p className="ptext">Hi! My Name is Sergio Lopez, a software engineer based in the bay area.</p>
+                            <p className="ptext">I decided to get into the tech world because I consider myself as a problem solver and I like to feel challenged with new projects, also I love to spend time with computers, create web applications to make people life easier or find the better way to optimize any task, so I decided to learn more about how to create software and applications on the internet.</p>
+                            <p className="ptext">I am currently studying the web developer boot camp at Berkeley Extension, I'll finish it at the end of August 2020. I am really happy with the skills that I have learned and I can not wait to apply them in my future job, I have learned skills like AJAX, API, Bootstrap, Cascading Style Sheets (CSS), ES6, Express.js, Git, GitHub, Heroku, HTML5, JavaScript, jQuery, JSON, Node.js, Object-oriented Programming (OOP), Rest API, SQL and React. </p>
+                            <p className="ptext">I am currently looking for an entry-level position where I can find opportunities to expand my knowledge and work on challenging projects</p>
                         </div>
                     </div>
                 </div>
