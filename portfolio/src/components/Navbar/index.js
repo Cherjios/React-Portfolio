0a574df62086011769f43c41663c032bb9b8453a
cherjios <slopez0286@gmail.com>
2020-08-19 14:50:32 -0700

    Adding home route to navbar

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -9,7 +9,7 @@ function Navbar() {
 
 
         <nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center ncolor">
-            <Link to="/" className="navbar-brand d-flex w-50 mr-auto"><samp className="text-white title-font">Sergio Lopez
+            <Link to="/React-Portfolio/" className="navbar-brand d-flex w-50 mr-auto"><samp className="text-white title-font">Sergio Lopez
         Murillo</samp></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
                 <span className
