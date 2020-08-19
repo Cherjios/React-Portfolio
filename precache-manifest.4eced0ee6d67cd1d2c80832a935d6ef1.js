c88384437186aa6840fceaa9d60d0d232d0efd10
cherjios <slopez0286@gmail.com>
2020-08-19 14:36:22 -0700

    Updates

---
+++ precache-manifest.4eced0ee6d67cd1d2c80832a935d6ef1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8dca29420af2923fab4144a008c24a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "899f473536d8695f2d13",
+    "url": "/React-Portfolio/static/css/main.33e316eb.chunk.css"
+  },
+  {
+    "revision": "4ea9b125bcc07a777f78",
+    "url": "/React-Portfolio/static/js/2.89723152.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.89723152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899f473536d8695f2d13",
+    "url": "/React-Portfolio/static/js/main.d71cc157.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "d84e7ec9092a24feb33c7b3e4b65cc91",
+    "url": "/React-Portfolio/static/media/SergioLopez_Resume.d84e7ec9.pdf"
+  },
+  {
+    "revision": "0e1ed516aa2240d737b92a9d115ad016",
+    "url": "/React-Portfolio/static/media/selfi.0e1ed516.jpeg"
+  }
+]);
