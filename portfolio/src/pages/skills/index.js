1d5ec4fedf6e68815a142a5f78d4ce6ff1ccb155
cherjios <slopez0286@gmail.com>
2020-09-23 11:41:41 -0500

    Add skills to portfolio

--- portfolio/src/pages/skills/index.js
+++ portfolio/src/pages/skills/index.js
@@ -4,10 +4,21 @@ import "./style.css"
 function Skills (){
     return(
         <div>
-            <h1>Skills</h1>
-            <p>
-                hello world
-            </p>
+            <div className="jumbotron jumbotron-fluid jcolor">
+                <div className="container text-center">
+                    <h1 className="font-weight-bolder title-font ah1"> Technical Skills</h1>
+                    {/* <p className="ap1">Take a look to the work that I have done!</p> */}
+                </div>    
+            </div>
+
+            <div className="container bjdk">
+            <h3 className="titleSk">Languages</h3>
+            <p className="subtitle">JavaScript, Python, ES6, Cascading Style Sheets (CSS), HTML5.</p>
+
+            <h3 className="titleSk">Applications</h3>
+            <p className="subtitle"> AJAX, API, Bootstraps, Express.js, Git, GitHub, Heroku, jQuery, JSON, Node.js, Object-oriented Programming (OOP), Rest API, SQL, React, JSX, Mongo DB, Mongoose ODM, MySQL, ORM, Sequelize.</p>
+            <br />
+            </div>
 
         </div>
 
